20888a29c72d3069a773bf4dda908719ff00c508
schillerm <schiller37@gmail.com>
2014-11-21 10:35:01 +0000

    Added basic classes

---
+++ MilkShakes/Models/Entities/MilkshakeDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+
+namespace MilkShakes.Models.Entities
+{
+    public class MilkshakeDB
+    {
+        public DbSet<Milkshake> Milkshakes { get; set; }
+        public DbSet<Milk> Milks { get; set; }
+        public DbSet<Fruit> Fruits { get; set; }
+    }
+}
